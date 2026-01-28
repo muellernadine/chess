d1bcf64e0d8c99a758d3865edb1c451890912dee
Nadine Müller <nadine.selina.mueller@gmail.com>
2026-01-28 07:03:33 +0100

    Added indexing dict to properly index the nested lists for the chess board

--- board.py
+++ board.py
@@ -7,40 +7,63 @@ from king import King
 
 class Board:
     def __init__(self):
-        self.rows = []
+        self.board = []
         for i in range(8):
             row = []
             for j in range(8):
                 col = " "
                 row.append(col)
-            self.rows.append(row)
+            self.board.append(row)
+        # Index to algebraically label chess board
+        self.row = {
+            8: 0,
+            7: 1,
+            6: 2,
+            5: 3,
+            4: 4,
+            3: 5,
+            2: 6,
+            1: 7 
+        }
+        self.col = {
+            "A": 0,
+            "B": 1,
+            "C": 2,
+            "D": 3,
+            "E": 4,
+            "F": 5,
+            "G": 6,
+            "H": 7
+        }
 
-    def print_board(self):
-        print("   ---------------------------------")
-        for i in range(7,-1,-1):
-            print(f""" {i+1} | {self.rows[i][0]} | {self.rows[i][1]} | {self.rows[i][2]} | {self.rows[i][3]} | {self.rows[i][4]} | {self.rows[i][5]} | {self.rows[i][6]} | {self.rows[i][7]} |""")
+
+    def print_board(self, active_colour):
+        if active_colour == "white":
             print("   ---------------------------------")
-        print('     A   B   C   D   E   F   G   H  ')
+            for row_index, row in self.row.items():
+                print(f""" {row_index} | {self.board[row][self.col["A"]]} | {self.board[row][self.col["B"]]} | {self.board[row][self.col["C"]]} | {self.board[row][self.col["D"]]} | {self.board[row][self.col["E"]]} | {self.board[row][self.col["F"]]} | {self.board[row][self.col["G"]]} | {self.board[row][self.col["H"]]} |""")
+                print("   ---------------------------------")
+            print('     A   B   C   D   E   F   G   H  ')
 
     def init_game(self):
 
         for i in range(8):
-            self.rows[6][i] = Pawn("white")
-            self.rows[1][i] = Pawn("black")
+            self.board[6][i] = Pawn("white")
+            self.board[1][i] = Pawn("black")
 
         colour = "black"
         for i in [0,7]:
-            self.rows[i][0] = Rook(colour)
-            self.rows[i][1] = Knight(colour)
-            self.rows[i][2] = Bishop(colour)
-            self.rows[i][3] = Queen(colour)
-            self.rows[i][4] = King(colour)
-            self.rows[i][5] = Bishop(colour)
-            self.rows[i][6] = Knight(colour)
-            self.rows[i][7] = Rook(colour)
+            self.board[i][0] = Rook(colour)
+            self.board[i][1] = Knight(colour)
+            self.board[i][2] = Bishop(colour)
+            self.board[i][3] = Queen(colour)
+            self.board[i][4] = King(colour)
+            self.board[i][5] = Bishop(colour)
+            self.board[i][6] = Knight(colour)
+            self.board[i][7] = Rook(colour)
             colour = "white"
 
 if __name__=="__main__":
     board1 = Board()
     board1.init_game()
-    board1.print_board()
+    board1.print_board("white")
